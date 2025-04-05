d45c1ca09436957e214f35a891ff5882750a2e7d
SMLDMS <smldms.eth@gmail.com>
2025-04-05 16:53:04 +0200

    smldmsX

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import { request, gql } from 'graphql-request';
 
-const endpoint = 'http://smldms.xyz/graphql';
+const endpoint = 'https://smldms.xyz/graphql';
 
 export const getAllCategories = async () => {
   const query = gql`
